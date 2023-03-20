32df9f9b4e1382ff3b64afd696df06ce8093ab3c
최 웅수 <infamousgames@hanmail.net>
2023-03-20 23:34:37 +0900

    Changes

---
+++ src/chap_01/_06_Constants.java
@@ -0,0 +1,18 @@
+package chap_01;
+
+public class _06_Constants {
+    public static void main(String[] args) {
+        String KR_COUNTRY_CODE = "+82"; //국가 번호
+        KR_COUNTRY_CODE = "+8282";
+        System.out.println(KR_COUNTRY_CODE);
+
+
+        final String KR_COUNTRY_CODE1 = "+82"; //국가 번호
+        //KR_COUNTRY_CODE1 = "+8282";
+        //System.out.println(KR_COUNTRY_CODE1);
+
+        final double RI = 3.141592; //원주율
+        final String DATE_OF_BIRTH = "2001-12-31"; //생년월일
+
+    }
+}
