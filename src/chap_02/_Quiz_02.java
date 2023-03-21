fdb1fdfb140e4cfcbecb567f6dfd69c879acb733
withus21-kykim <woungsoo94@gmail.com>
2023-03-21 10:16:01 +0900

    Changes

---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,15 @@
+package chap_02;
+// 키가 115cm 이므로 탑승 불가능
+// 키가 121cm 이므로 탑승 가능
+public class _Quiz_02 {
+    public static void main(String[] args) {
+        int height = 115;
+        String result = (height >= 120) ? "탑승 가능" : "탑승 불가능";
+        System.out.println("키가" + height + "cm 이므로 " + result);
+
+        height = 121;
+        result = (height >= 111) ? "탑승 가능" : "탑승 불가능";
+        System.out.println("키가" + height + "cm 이므로 " + result);
+
+    }
+}
